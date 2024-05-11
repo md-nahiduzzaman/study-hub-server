d83164daeedf7a14954ff19c394001120d1f8e88
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-05-12 00:59:16 +0600

    added my submission get function

--- index.js
+++ index.js
@@ -88,6 +88,14 @@ async function run() {
       res.send(result);
     });
 
+    // get submitted assignment by email from db
+    app.get("/my-submission/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await submittedCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
