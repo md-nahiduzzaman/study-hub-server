8751acb7de45cbfc367169901fb6868198dcd728
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-05-12 16:37:13 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // config
@@ -10,7 +12,8 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
   })
 );
 app.use(express.json());
@@ -32,6 +35,21 @@ async function run() {
       .db("studyHub")
       .collection("assignments");
     const submittedCollection = client.db("studyHub").collection("submitted");
+
+    // jwt generator
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // get all assignment data from db
     app.get("/assignments", async (req, res) => {
